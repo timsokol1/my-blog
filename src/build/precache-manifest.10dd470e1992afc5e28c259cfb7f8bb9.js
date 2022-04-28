d8ca47d14af20ad0c588593f6d6b9fbfa82e7d84
timsokol1 <104537338+timsokol1@users.noreply.github.com>
2022-04-27 22:00:03 -0400

    Add files via upload

---
+++ src/build/precache-manifest.10dd470e1992afc5e28c259cfb7f8bb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25a7b5dc108a9c5bcf83",
+    "url": "/static/css/main.9599f012.chunk.css"
+  },
+  {
+    "revision": "25a7b5dc108a9c5bcf83",
+    "url": "/static/js/main.f8793272.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c41ad2443ecee5c769f3",
+    "url": "/static/js/2.ba5cc21a.chunk.js"
+  },
+  {
+    "revision": "dd7c46169fff7ee4fdde7862cf05f0a9",
+    "url": "/index.html"
+  }
+];
